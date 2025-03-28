ad0b9d74565a197f4e77f3f77bbee08e916575bb
Youssef Fawel <131284312+Youssef-Fawel@users.noreply.github.com>
2025-03-29 00:37:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -111,10 +111,9 @@ const getEventTimes = () => {
   const now = new Date();
   
   const imsakTime = new Date();
-  imsakTime.setHours(0, 38, 0, 0);
-  if (now.getHours() > 0 || (now.getHours() === 0 && now.getMinutes() >= 38 )) {
+  imsakTime.setHours(4, 34, 0, 0);
+  if (now.getHours() > 4 || (now.getHours() === 4 && now.getMinutes() >= 34 )) {
     imsakTime.setDate(imsakTime.getDate() + 1);
-  }
   
   const iftarTime = new Date();
   iftarTime.setHours(18, 46, 0, 0);
